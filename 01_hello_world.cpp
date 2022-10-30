96416267a05062dc7ee00806b3f82bd3131de319
MAG <magjpmag@gmail.com>
2022-10-30 23:34:29 +0530

    Initial Commit

---
+++ 01_hello_world.cpp
@@ -0,0 +1,8 @@
+#include <iostream>  // header file: for std::cout
+using namespace std; // using namespace: for std::cout
+
+int main(int argc, char const *argv[])
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
